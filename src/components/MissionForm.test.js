4ee04234f7037d3e7d76a4c4f64d966ebf5e3e22
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-08 12:25:18 -0500

    lecture work-in-progress

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -1,2 +1,5 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import MissionForm from './MissionForm';
+
