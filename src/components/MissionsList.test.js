cf4631096882f5e8b643437ecc4480bd5fa95f9e
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-08 13:53:00 -0500

    lecture work-in-progress

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -5,3 +5,14 @@ import MissionsList from './MissionsList';
 test("renders without errors", ()=> {
     render(<MissionsList missions={[]}/>);
 });
+
+test("renders 3 missions after rendering no missions", ()=> {
+    //Arrange 1: render component with no missions
+    render(<MissionsList missions={[]}/>);
+    //Act 1: Find all missions
+    //Asset 1: Check that no missions have been rendered.
+
+    //Arrange 2: render component with 3 missions
+    //Act 3: Find all missions
+    //Asset 3: Check that 3 missions have been rendered.
+});
