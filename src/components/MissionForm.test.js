668d4e4b14be88aec7e399317e55f945f866594c
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-08 12:49:36 -0500

    lecture work-in-progress

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -18,14 +18,14 @@ test('renders loading message if isFetchingData is true', ()=> {
     //Assert: Verify that loading message exists
     expect(loadingMessage).toBeInTheDocument();
     expect(button).not.toBeInTheDocument();
-    
+
     // expect(loadingMessage).toBeTruthy();
     // expect(loadingMessage).not.toBeNull();
     // expect(loadingMessage).toHaveTextContent('we are fetching data');
 });
 
 test('renders button if isFetchingData is false', ()=> {
-
+    
 })
 
 test('executes getData if button is clicked', ()=> {
