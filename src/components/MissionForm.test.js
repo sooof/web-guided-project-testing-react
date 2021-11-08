40a93f5454a366f2abafc62d23960c7f939a506b
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-08 13:12:39 -0500

    lecture work-in-progress

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -35,7 +35,9 @@ test('renders button if isFetchingData is false', ()=> {
 })
 
 test('executes getData if button is clicked', ()=> {
-    const fakeGetData = jest.fn();
+    const fakeGetData = jest.fn(()=> {
+        console.log("doing the fake function thing.");
+    });
 
     //Arrange: Render our component with isFetchingData === false
     render(<MissionForm isFetchingData={false} getData={fakeGetData}/>);
