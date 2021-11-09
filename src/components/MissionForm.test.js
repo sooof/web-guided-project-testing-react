245e4c7910538556c1631b636db9f481bb715883
sooof <sooof@me.com>
2021-11-08 19:18:00 -0800

    test 4

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -5,5 +5,60 @@ import MissionForm from "./MissionForm";
 
 test("render without errors", ()=>{
     render(<MissionForm/>);
-    
+});
+
+
+test('renders loading message if isFetchingData is true', ()=> {
+    //Arrange: Renders our component with isFetchingData === true
+    render(<MissionForm isFetchingData={true}/>);
+
+    //Act: Find our loading message
+    const loadingMessage = screen.queryByText(/we are fetching data/i);
+    const button = screen.queryByRole('button');
+
+    //Assert: Verify that loading message exists
+    expect(loadingMessage).toBeInTheDocument();
+    expect(button).not.toBeInTheDocument();
+
+     expect(loadingMessage).toBeTruthy();
+     expect(loadingMessage).not.toBeNull();
+     expect(loadingMessage).toHaveTextContent('we are fetching data');
+});
+test('renders button if isFetchingData is false', ()=> {
+    render(<MissionForm isFetchingData={false}/>);
+
+    const loadingMessage = screen.queryByText(/we are fetching data/i);
+    const button = screen.queryByRole("button");
+
+    expect(loadingMessage).toBeFalsy();
+    expect(button).toBeTruthy();
 })
+test("render  button if  isFatchingData is false 1", ()=>{
+    render(<MissionForm isFetchingData={false} />)
+
+    const loadingMessage = screen.queryByAltText(/we are fetching data/i)
+    const button = screen.queryByRole('button');
+    
+    expect(loadingMessage).toBeFalsy();
+});
+test('executes getData if button is clicked', ()=> {
+    // const fakeGetData = () => {
+    //     console.log("Calling my fake function")
+    // }
+    const fakeGetData = jest.fn();
+
+    //Arrange: Render our component with isFetchingData === false
+    render(<MissionForm isFetchingData={false} getData={fakeGetData}/>);
+
+    //Act: Find our button. Press our button.
+    const button = screen.queryByRole("button");
+    userEvent.click(button);
+
+    //Assert: See if a function gets called?
+    console.log(fakeGetData.mock)
+    expect(fakeGetData).toHaveBeenCalled();
+    // expect(fakeGetData.mock.calls.length).toBe(1);
+    // expect(fakeGetData.mock.calls.length === 1).toBeTruthy();
+    // expect(fakeGetData.mock.calls).toHaveLength(1);
+    // expect(fakeGetData).toHaveBeenCalledTimes(1);
+});
