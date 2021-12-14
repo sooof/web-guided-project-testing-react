29212b0ea977d4827122f946415ec798d058cce8
sooof <sooof@me.com>
2021-12-13 20:33:15 -0800

    App.test.js : use async await can make test work #2

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,8 @@ import { screen, render} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from './App';
 
-// import { fetchMissions } from './api/fetchMissions';
-// jest.mock('./api/fetchMissions');
+import { fetchMissions } from './api/fetchMissions';
+jest.mock('./api/fetchMissions');
 
 
 test("renders without errors", ()=> {
@@ -12,7 +12,13 @@ test("renders without errors", ()=> {
 });
 
 test("Renders all missions that are returned from fetchMissions",  async () => {
-
+    fetchMissions.mockResolvedValueOnce({
+        data: [
+            {mission_id: 1, mission_name: "test 1"},
+            {mission_id: 2, mission_name: "test 2"},
+            {mission_id: 3, mission_name: "test 3"},
+        ]
+    });
 
     //Arrange: render our App component
     render(<App/>);
@@ -26,5 +32,5 @@ test("Renders all missions that are returned from fetchMissions",  async () => {
     //  get all items with testid mission and check for 10 missions
     const missions = await screen.findAllByTestId("mission");
     console.log(missions)
-    expect(missions).toHaveLength(10);
+    expect(missions).toHaveLength(3);
 });
